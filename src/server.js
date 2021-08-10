d4455cf1339f81316d58f5226139a4082697b4f9
haglea <haglea@icloud.com>
2021-08-10 18:08:45 +0200

    fixed server.use

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ import listEndpoints from "express-list-endpoints"
 
 const server = express();
 const port = 3001;
-server.use("/authors", authorsRouter)
+
 server.use(express.json())
+server.use("/authors", authorsRouter)
+
 console.table(listEndpoints(server))
 server.listen(port, () => {
   console.log(`Server running at ${port}/`);
